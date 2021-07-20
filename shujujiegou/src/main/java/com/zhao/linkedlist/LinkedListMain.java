1ab5a5fc080ae98b915f4f5a2c0504a7ff447c2d
zhendiao <351848327@qq.com>
2021-07-20 08:36:18 +0800

    add linklist

---
+++ shujujiegou/src/main/java/com/zhao/linkedlist/LinkedListMain.java
@@ -0,0 +1,13 @@
+package com.zhao.linkedlist;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i=0;i<5;i++){
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+        linkedList.add(2,666);
+        System.out.println(linkedList);
+    }
+}
